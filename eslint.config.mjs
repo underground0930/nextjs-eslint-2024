f4ecbb07a440215d0ee3883e1eee99928e620755
underground0930 <go.nishiduka.1985@gmail.com>
2024-10-28 14:00:16 +0900

    format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,74 @@
-export default [];
+import nextPlugin from '@next/eslint-plugin-next'
+import reactPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+const config = [
+  {
+    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+  },
+  {
+    ignores: [
+      '**/eslint.config.mjs',
+      '**/prettier.config.js',
+      '**/next.config.mjs',
+      '**/tailwind.config.js',
+      '**/tsconfig.json',
+      '**/postcss.config.mjs',
+      '**/next-env.d.ts',
+      '**/build/',
+      '**/bin/',
+      '**/obj/',
+      '**/out/',
+      '**/.next/',
+    ],
+  },
+  {
+    name: 'eslint/recommended',
+    rules: js.configs.recommended.rules,
+  },
+  ...tseslint.configs.recommended,
+  {
+    name: 'eslint-plugin-react',
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: reactPlugin.configs['jsx-runtime'].rules,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    name: 'eslint-plugin-react-hooks',
+    plugins: {
+      'react-hooks': hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  {
+    name: 'eslint-plugin-next',
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    name: 'eslint-config-prettier',
+    ...eslintConfigPrettier,
+  },
+  {
+    name: 'eslint-custom', // カスタムルールを追加
+    rules: {
+      '@typescript-eslint/no-unused-vars': 1,
+    },
+  },
+]
+
+export default config
